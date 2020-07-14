0208135f4951d9a0ced912f1f5db9fa9d099b72d
George H. Allen <georgehenryallen@gmail.com>
2020-07-14 13:47:52 -0500

    Code used to send V1 product to Alberto Borges

--- src/finalTableGenerator.R
+++ src/finalTableGenerator.R
@@ -5,6 +5,12 @@
 # George Allen, June-July 2020
 
 
+# join data from Tomin to lake polygons 
+# global lakeshed polygons to lake polygons (geographic area) 
+# join coverage data from dbfs to lake polygons 
+# join lake polygons for additional attributes $hylak_id 
+
+
 
 ################################################################################ 
 library("foreign")
@@ -15,43 +21,38 @@ inDir = paste0(wd, "/in")
 outDir = paste0(wd, "/out")
 
 
-
 # Toming DOC path:
 tomingPath = paste0(inDir, "/Toming_DOC/Toming_et_al_global_DOC_DATA.txt")
 
 # lake polygon shapefile path:
 lakePolypath = paste0(inDir, "/hydroLAKES/hydroLAKES_polys/HydroLAKES_polys_v10_tropicsClip.dbf")
-
+lakePolyOutpath = paste0(outDir, "/HydroLAKES_polys/HydroLAKES_polys_v10_tropicsClip.dbf")
 
 # lakeShed path:
 lakeShedsDBFpath = paste0(inDir, "/lakeSheds/hydroSHEDS_polygon_tropics.dbf")
-
+lakeshedOutpath = paste0(outDir, "/lakeSheds/hydroSHEDS_polygon_tropics.dbf")
 
 # lakeshed GIEMS paths:
 shedCoverageDir = paste0(inDir, "/lakeShed_landCover")
-
-contList = c("af", "as", "au", "ca", "eu", "na", "sa")
-covList = c("GIEMS", "TCI", "WTD", "RFW")
-outShedCoverageCSVpaths = paste0(outDir, "/CSV/", contList, ".csv")
 allShedCoveragePaths = list.files(shedCoverageDir, ".dbf", recursive=T, full.names=T)
+shedCoverageOutpath = paste0(outDir, "/lakeShed_landCover/lakeShed_GIEMS.csv")
 
+# worldclim climate data:
+precipPath = paste0(inDir, "/hydroLAKES_worldclim_mean_DBFs/HydroLAKES_precip.dbf")
+tempPath = paste0(inDir, "/hydroLAKES_worldclim_mean_DBFs/HydroLAKES_temp.dbf")
+windPath = paste0(inDir, "/hydroLAKES_worldclim_mean_DBFs/HydroLAKES_wind.dbf")
 
 # lake pourPoint shapefile path:
 lakePPpath = paste0(inDir, "/hydroLAKES/hydroLAKES_points/HydroLAKES_points_v10_tropicsClip.dbf")
 
 
-# want to make a map of lakesheds and their land cover type --> need to join lake attributes to lake sheds
-# want to make a map of lake polygons and their climatology --> need to join shed attributes to lakes
-
-# join data from Tomin to lake polygons --> $hylak_id 
-# global lakeshed polygons to lake polygons (get geographic area) --> $gridcode 
-# join coverage data from dbfs to lake polygons  --> $HydroLAKES_id
 
 
-# join lake polygons for additional attributes $hylak_id 
 
 
-# remove reservoirs from lakes table and send to Alberto 
+
+
+
 
 ################################################################################  
 # Join Toming DOC data to lake polygon:
@@ -76,6 +77,28 @@ lakePoly = data.frame(lakePoly, joinTab[j,])
 # "Toming_5", "Toming_6", "Toming_7", "Toming_8" --> refer to columns in Toming table
 
 
+
+################################################################################ 
+# join climatology data to lake polygons:
+
+precip = read.dbf(precipPath)
+j = match(lakePoly$Hylak_id, precip$Hylak_id)
+prec_mon_mm = precip$MEAN[j]
+lakePoly = data.frame(lakePoly, prec_mon_mm)
+
+
+temp = read.dbf(tempPath)
+j = match(lakePoly$Hylak_id, temp$Hylak_id)
+temp_C = temp$MEAN[j]
+lakePoly = data.frame(lakePoly, temp_C)
+
+wind = read.dbf(windPath)
+j = match(lakePoly$Hylak_id, wind$Hylak_id)
+wind_mps  = wind$MEAN[j]
+lakePoly = data.frame(lakePoly, wind_mps)
+
+
+
 ################################################################################  
 # Join lakeShed areas to lake polygon:
 
@@ -95,42 +118,13 @@ lakePoly = data.frame(lakePoly, lakeShedArea_km2)
 
 
 ################################################################################  
-# calculate lake shed wetland coverage: 
+# calculate lake shed wetland coverage 
+# (~1 min run time --> export written to disk):
 
-for (i in 1:length(contList)){ #i = 1
+# concatenate all contents into a global tropical table:
+for (i in 1:length(allShedCoveragePaths)){ 
   
-  contPaths = grep(contList[i], allShedCoveragePaths, value=T)
-  
-  for (j in 1:length(covList)){ #j = 1
-  
-    covPath = grep(covList[j], contPaths, value=T)
-    
-    covDF = foreign::read.dbf(covPath)
-    
-    # first column of data frame is the index of watershed. 
-    # remove first column and normalize other columns:
-    covDF_vals = covDF[,-1]
-    rowSums = rowSums(covDF_vals)
-    covDF_vals_norm = sweep(covDF_vals, 1, "/", STATS=rowSums(covDF_vals))
-    
-    names(covDF_vals_norm) = paste0(covList[j], "_", 0:(ncol(covDF_vals_norm)-1))
-    
-    if (j==1){ 
-      covDF_out = data.frame(covDF[,1], covDF_vals_norm)
-      names(covDF_out)[1] = "hylak_id"
-    }else{
-      
-      # HydroLAKES IsD match between coverage layers:
-      covDF_out = data.frame(covDF_out, covDF_vals_norm)
-    }
-  }
-  
-  write.csv(covDF_out, outShedCoverageCSVpaths[i], row.names=F)
-}
-
-# rbind all land cover tables together:
-for (i in 1:length(outShedCoverageCSVpaths)){
-  covDF = read.csv(outShedCoverageCSVpaths[i], header=T)
+  covDF = read.dbf(allShedCoveragePaths[i])
   
   if (i == 1){
     covDF_glob = covDF
@@ -140,74 +134,97 @@ for (i in 1:length(outShedCoverageCSVpaths)){
   
 }
 
-# average land cover of duplicated hybas:
-dupI = which(duplicated(covDF_glob$hylak_id))
+# rename column headers:
+names(covDF_glob) = sub("VALUE", "GIEMS", names(covDF_glob))
+names(covDF_glob)[1] = "hylak_id"
 
-for (i in 1:length(dupI)){
-  j = which(covDF_glob$hylak_id[dupI[i]] == covDF_glob$hylak_id)
-  colMeans(covDF_glob[j,])
+# average land cover of duplicated hylak_id:
+dupIDs = covDF_glob$hylak_id[duplicated(covDF_glob$hylak_id)]
+
+for (i in 1:length(dupIDs)){
+  j = which(dupIDs[i] == covDF_glob$hylak_id)
+  
+  if (length(j)==1){ next }
+  
+  # change first instance of duplicated ID to mean, delete others:
+  covDF_glob[j[1],] = colMeans(covDF_glob[j,])
+  
+  covDF_glob[j[-1],] = NA
+  
 }
+
+dim(covDF_glob)
+covDF_glob = na.omit(covDF_glob)
+dim(covDF_glob)
+
+
+# normalize land cover data so they add to unity:
+covDF_vals = covDF_glob[,-1]
+rowSums = rowSums(covDF_vals)
+covDF_vals_norm = sweep(covDF_vals, 1, "/", STATS=rowSums(covDF_vals))
+covDF_glob[, grep("GIEMS", names(covDF_glob))] = covDF_vals_norm
+
+
+write.csv(covDF_glob, shedCoverageOutpath, row.names=F)
 
 
 
 ################################################################################
 # join lake shed GIEMS land cover data to lake polygons
 
+# lake shed land cover data:
+covDF = read.csv(shedCoverageOutpath, header=T)
+
+joinTab = covDF[, grep("GIEMS", names(covDF))]
+
+j = match(lakePoly$Hylak_id, covDF$hylak_id)
+
+lakePoly = data.frame(lakePoly, joinTab[j,])
+
+# write out joined lake poly table:
+write.dbf(lakePoly, lakePolyOutpath)
 
 
 
-length()
-  
-  # only need to use GIEMS data:
-  giems_cols = c(1, grep("GIEMS", names(covDF)))
-  
-  joinTab = covDF[, giems_cols]
-  
-  # join toming DOC data to lake polygon table:
-  j = match(lakePoly$Hylak_id, covDF$hylak_id)
-  
-  if (i == 1){
-    lakePoly = data.frame(lakePoly, joinTab[j,])
-  }else{
-    names(lakePoly)
-  }
-  
-}
+write.csv(lakePoly, sub(".dbf", ".csv", lakePolyOutpath), row.names=F)
 
 
 
-################################################################################ 
-# join coverage data to lakeShed shapefiles:
-lSheds = read.dbf(lakeShedsDBFpath)
 
-names(lSheds)[names(lSheds) == 'gridcode'] = "hylak_id"
-bindTab = as.data.frame(array(NA, c(nrow(lSheds, 5))))
+################################################################################
+# Join attributes to the lake shed shapefile (for display purposes):
 
-for (i in 1:length(outShedCoverageCSVpaths)){
-  covDF = read.csv(outShedCoverageCSVpaths[i], header=T)
-  
-  # only need to use GIEMS data:
-  giems_cols = c(1, grep("GIEMS", names(covDF)))
-  
-  #joinInd = match(lSheds$hylak_id, covDF$HydroLAKES_ID)
-  
-  joinInd = match(covDF$HydroLAKES_ID, lSheds$hylak_id)
-  
+# read in lakeshed data:
+lakeshed = read.dbf(lakeShedsDBFpath)
+names(lakeshed) = c("Id", "hylak_id", "lakeShedArea_km2")
 
 
-  bindTab[joinInd] =  covDF[,giems_cols]
-  
-  lSheds$gridcode[62077]
-  covDF[joinInd] 
-  
-}
+# read in lake poly out table:
+lakePoly = read.dbf(lakePolyOutpath)
+
+joinTab = lakePoly[, grep("GIEMS", names(lakePoly), invert=T)]
+
+j = match(lakeshed$hylak_id, lakePoly$Hylak_id)
+
+lakeshed = data.frame(lakeshed, joinTab[j,])
 
 
 
-lSheds$gridcode
+# lake shed land cover data intentionally seperately than from lake poly table 
+# b/c GIEM basins might have been dropped in lake poly table join above:
+covDF = read.csv(shedCoverageOutpath, header=T)
 
-################################################################################ 
-# join lake area data: 
+joinTab = covDF[, grep("GIEMS", names(covDF))]
+
+
+# join GIEMS data to lake sheds:
+j = match(lakeshed$hylak_id, covDF$hylak_id)
+
+lakeshed = data.frame(lakeshed, joinTab[j,])
+
+
+# write out joined lake shed table:
+write.dbf(lakeshed, lakeshedOutpath)
 
 
 
